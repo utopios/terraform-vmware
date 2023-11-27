516aaec5927b305e97e83ec3d3b2163bd8687001
ihab abadi <ihab@utopios.net>
2023-11-27 16:34:26 +0100

    exemple de création de vswith vmware

--- demo_provider_vmware/main.tf
+++ demo_provider_vmware/main.tf
@@ -32,14 +32,34 @@ provider "vsphere" {
     allow_unverified_ssl = true
 }
 
+
+## Récupération des informations sur la resource datacenter
 data "vsphere_datacenter" "datacenter" {
   name = "Datacenter"
 }
 
+
+## Récupération des informations sur la resource network
 data "vsphere_network" "network" {
   name = "VM Network"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
+
+## Récupération des informations sur la resource host
+
+data "vsphere_host" "host" {
+    name = "192.168.29.130"
+    datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+## Création d'in vSwitch
+resource "vsphere_host_virtual_switch" "switch" {
+    name = "name_virtual_switch"
+    host_system_id = "${data.vsphere_host.host.id}"
+    network_adapters = ["vmnic0"]
+    active_nics = ["vmnic0"]
+}
+
 
 output "information_network_type" {
   value = data.vsphere_network.network.type
